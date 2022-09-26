e80fe2b77d57b626ec32ffddd1c7f5d48cffbc5d
QuangNH <quangnh@vnpt-technology.vn>
2022-09-26 09:27:34 +0700

    create converter for room database

---
+++ app/src/main/java/com/example/dictionaryappcleanarchitecture/data/data_source/local/Converters.kt
@@ -0,0 +1,29 @@
+package com.example.dictionaryappcleanarchitecture.data.data_source.local
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.example.dictionaryappcleanarchitecture.data.util.JsonParser
+import com.example.dictionaryappcleanarchitecture.domain.model.Meaning
+import com.google.gson.reflect.TypeToken
+
+@ProvidedTypeConverter
+class Converters(
+    private val jsonParser: JsonParser
+) {
+    @TypeConverter
+    fun fromMeaningsJson(json: String): List<Meaning> {
+        return jsonParser.fromJson<ArrayList<Meaning>>(
+            json,
+            object : TypeToken<ArrayList<Meaning>>(){}.type
+        ) ?: emptyList()
+    }
+
+    @TypeConverter
+    fun toMeaningJson(meanings: List<Meaning>): String {
+        return jsonParser.toJson(
+            meanings,
+            object : TypeToken<ArrayList<Meaning>>(){}.type
+        ) ?: "[]"
+    }
+
+}
--- app/src/main/java/com/example/dictionaryappcleanarchitecture/data/data_source/local/WordInfoDatabase.kt
+++ app/src/main/java/com/example/dictionaryappcleanarchitecture/data/data_source/local/WordInfoDatabase.kt
@@ -2,12 +2,14 @@ package com.example.dictionaryappcleanarchitecture.data.data_source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.dictionaryappcleanarchitecture.data.data_source.local.entity.WordInfoEntity
 
 @Database(
     entities = [WordInfoEntity::class],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class WordInfoDatabase: RoomDatabase() {
     abstract fun wordInfoDao(): WordInfoDao
 }
